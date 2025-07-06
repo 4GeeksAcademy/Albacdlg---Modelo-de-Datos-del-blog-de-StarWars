9c46e68e17bb7f49571a059c8b32264d9b6134b8
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-07-06 12:17:04 +0000

    Clase User ampliada con campos adicionales

--- src/models.py
+++ src/models.py
@@ -4,16 +4,24 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    susbscription_date: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_nem": self.last_name,
+            "susbscription_date": self.susbscription_date.isoformat()
             # do not serialize the password, its a security breach
         }
